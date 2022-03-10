7b7469a30745580f187de257892fcc77bf1966a0
rmhsh8 <99620775+rmhsh8@users.noreply.github.com>
2022-03-10 13:26:17 -0800

    Add files via upload

--- timeit.py
+++ timeit.py
@@ -1,15 +1,16 @@
 import time
 def calculate_time(func):
-    def wrapper():
-        x = func()
-        input()
-        y = func()
-        print('Total Time', y-x, 'seconds')
+    def wrapper(*args, **kw):
+        x = time.time()
+        result = func(*args, **kw)
+        y = time.time()
+        print(f'Total Time', int(y-x), 'seconds')
+        return result
     return wrapper
 
+@calculate_time
 def test():
-    return time.time()
+    for i in range (int(10e7)):
+        pass
 
-
-test = calculate_time(test)
 test()
