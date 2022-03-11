899b6c4a482b7709b9efe2968f8a61103e42423a
rmhsh8 <99620775+rmhsh8@users.noreply.github.com>
2022-03-10 21:40:09 -0800

    Add files via upload

--- double.py
+++ double.py
@@ -1,17 +1,10 @@
 def doubler(func):
     def math():
-        x = func()
-        return x ** 3
-    return math
-def dec(func):
-    def math():
-        x = func()
-        return x // 2
+        func()
+        func()
     return math
     
 @doubler
-@dec
 def solve():
-    x = 9
-    return x
-print(solve())
+    print("Hello, World")
+solve()
